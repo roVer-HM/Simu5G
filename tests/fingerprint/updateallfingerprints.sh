f39fc722c069866dd6998e6931fc855b7688bfed
Lars Wischhof <wischhof@ieee.org>
2024-05-24 18:18:40 +0200

    fix: consider tests in subdirectories

--- tests/fingerprint/updateallfingerprints.sh
+++ tests/fingerprint/updateallfingerprints.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# move *.UPDATED files
-for FILE in *.UPDATED; do
-    mv "$FILE" "${FILE%.UPDATED}" 
-done
+declare -a dirs=("LTE" "mec" "NR")
 
-# remove all .FAILED files 
-for FILE in *.FAILED; do
-    rm "$FILE" 
+for d in "${dirs[@]}"
+do
+    cd $d
+    # move *.UPDATED files
+    for FILE in *.UPDATED; do
+        mv "$FILE" "${FILE%.UPDATED}" 
+    done
+
+    # remove all .FAILED files 
+    for FILE in *.FAILED; do
+        rm "$FILE" 
+    done
+    cd ..
 done
